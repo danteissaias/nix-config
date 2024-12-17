58f0c950302078b8ed5cf9466b573c247c163e05
Dante Issaias <dante@issaias.com>
2024-12-17 11:35:26 +0000

    Add persistent dock apps

--- system.nix
+++ system.nix
@@ -1,17 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  system = {
-    defaults = {
-      menuExtraClock.Show24Hour = true;
-
-      dock = {
-        autohide = true;
-        show-recents = false;
-      };
-
-      finder = {
-        AppleShowAllFiles = true;
-      };
-    };
+  system.defaults = {
+    menuExtraClock.Show24Hour = true;
+    dock.autohide = true;
+    dock.show-recents = false;
+    dock.persistent-apps = [
+      "${pkgs.arc-browser}/Applications/Arc.app"
+      "${pkgs.kitty}/Applications/Kitty.app"
+      "/System/Applications/Mail.app"
+      "${pkgs.spotify}/Applications/Spotify.app"
+    ];
+    finder.AppleShowAllFiles = true;
+    NSGlobalDomain.AppleInterfaceStyle = "Dark";
+    NSGlobalDomain.KeyRepeat = 2;
   };
 }
