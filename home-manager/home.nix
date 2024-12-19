40fe12e9d2ef7e731aebe2f3dea2cfbdaa0a3c67
Dante Issaias <dante@issaias.com>
2024-12-19 11:51:28 +0000

    Git aliases

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -55,6 +55,17 @@
       set fish_cursor_replace_one underscore blink
       set fish_cursor_visual      block
     '';
+    shellAliases = {
+      ga = "git add";
+      gc = "git commit";
+      gco = "git checkout";
+      gcp = "git cherry-pick";
+      gdiff = "git diff";
+      gl = "git prettylog";
+      gp = "git push";
+      gs = "git status";
+      gt = "git tag";
+    };
   };
 
   programs.kitty = {
