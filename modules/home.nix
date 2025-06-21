449557074ceeb64f2035acf01438cc46bd3e3574
Dante Issaias <dante@issaias.com>
2025-06-21 09:16:18 +0100

    Add nh

--- modules/home.nix
+++ modules/home.nix
@@ -32,6 +32,13 @@ in
     ]
   );
 
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/etc/nix-darwin";
+  };
+
   xdg.enable = true;
   home.preferXdgDirectories = true;
 
