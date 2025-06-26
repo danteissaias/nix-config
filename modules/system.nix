ce0d5a4d7340c026715e848da41a7ef78c5d9cb2
Dante Issaias <dante@issaias.com>
2025-06-26 12:39:00 +0100

    Activate settings

--- modules/system.nix
+++ modules/system.nix
@@ -36,8 +36,8 @@
     ".GlobalPreferences"."com.apple.mouse.scaling" = 1.5;
   };
 
-  # system.activationScripts.postUserActivation.text = ''
-  #   # Following line should allow us to avoid a logout/login cycle
-  #   /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-  # '';
+  system.activationScripts.postActivation.text = ''
+    # Following line should allow us to avoid a logout/login cycle
+    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+  '';
 }
