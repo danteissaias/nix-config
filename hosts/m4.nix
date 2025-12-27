acd187498f11ec2d08151efd94eed2586cc325cb
Dante Issaias <dante@issaias.com>
2025-12-27 13:21:37 +0000

    Remove cachix

--- hosts/m4.nix
+++ hosts/m4.nix
@@ -1,15 +1,5 @@
-{ pkgs, ... }:
 {
   networking.computerName = "Dante's MacBook Pro (M4)";
   networking.hostName = "m4";
   system.stateVersion = 6;
-
-  services.cachix-agent = {
-    enable = true;
-    name = "m4";
-  };
-
-  environment.systemPackages = with pkgs; [
-    cachix
-  ];
 }
