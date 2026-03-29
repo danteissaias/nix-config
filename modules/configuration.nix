3697571f35bf2cb1d159c76c6f620a7cbbb8427c
Dante Issaias <dante@issaias.com>
2026-03-29 16:21:01 +0100

    Disable warn-dirty

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -15,6 +15,8 @@
   nix.settings = {
     experimental-features = "nix-command flakes";
     use-xdg-base-directories = true;
+    warn-dirty = false;
+
     substituters = [ "https://nix-community.cachix.org" ];
     trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
 
